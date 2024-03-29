51b231b2eb83fb62331454e4fd831c211cc3c760
Om Jaju <113230517+omjaju18@users.noreply.github.com>
2024-03-29 16:40:33 +0530

    Stripe

--- app/api/courses/[courseId]/checkout/route.ts
+++ app/api/courses/[courseId]/checkout/route.ts
@@ -11,7 +11,6 @@ export async function POST(
 ) {
   try {
     const user = await currentUser();
-    console.log("User:", user);
 
     if (!user || !user.id || !user.emailAddresses?.[0]?.emailAddress) {
       return new NextResponse("Unauthorized", { status: 401 });
